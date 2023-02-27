64dcf58585f903ebb1c6d98d118132f9d13b9923
testsnake <testsnake12@gmail.com>
2023-02-26 18:33:41 -0800

    fixed stuff

--- commands/clap.js
+++ commands/clap.js
@@ -4,6 +4,10 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('clap')
 		.setDescription('Adds 👏 Clap 👏 Emoji 👏 in 👏 between 👏 each 👏 word.'),
+		.addStringOption(option => 
+			option.setName('text')
+				.setDescription('The text you want to uwu-ify.')
+				.setRequired(true)),
 	async execute(interaction) {
 		const text = interaction.options.getString('text');
 		const clapText = str.replace(/ /g, ' 👏 ')
