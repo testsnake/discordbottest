571bf2f8c8d225f12b656f22207447acc4eeeece
testsnake <testsnake12@gmail.com>
2023-03-21 22:04:26 -0700

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -8,6 +8,16 @@ module.exports = {
     .setDescription('Restarts MikuBot'),
   async execute(interaction) {
     if (interaction.member.permissions.has('ADMINISTRATOR')) {
+      exec('sudo bash /home/testsnake/update.sh', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          const loggingChannelId = '1008978799989362808';
+          client.channels.fetch(loggingChannelId).send('MikuBot has had an error Updating\n```' + err + '```');
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
       exec('sudo bash /home/testsnake/github/discordbottest/reboot.sh', (error, stdout, stderr) => {
         if (error) {
           console.error(`exec error: ${error}`);
